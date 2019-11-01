c713a3d3a9a816ab7805219825c886dfd1e0569e
Circuit Specialists <github@circuitspecialists.com>
2019-11-01 12:12:56 -0700

    update

--- Micropython starter files/boot.py
+++ Micropython starter files/boot.py
@@ -1,18 +1,14 @@
-try:
-    exec(open("bash_commands.py").read())
-except:
-    pass
+import main
+import esp
+import webrepl
+import network
 
 try:
-    exec(open("main.py").read())
-    init()
-except:
-    ## recover board on failure of main script
-    import esp
-    import webrepl
-    import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(SSID, Password)
     utime.sleep(3)
     webrepl.start()
+    main_thread = main.MAIN()
+except:
+    pass
